7456550809992714bde174e3d73ce041cea5e0f0
Patoalcuadrado <diazruiz403@gmail.com>
2025-03-31 15:44:38 +0200

    Refactorizacion

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -12,7 +12,7 @@ import java.util.*;
  * @since 2025
  */
 public class Agenda {
-    private List<Contacto> contacts; // Lista de Contacto
+    private List<Persona> contacts; // Lista de Contacto
     /**
      * Constructor que inicializa la lista de contactos.
      */
@@ -29,7 +29,7 @@ public class Agenda {
 
     public void addContact(String name, String phone) {
         boolean exists = false;
-        for (Contacto c : contacts) {
+        for (Persona c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
                 exists = true;
                 c.getPhones().add(phone);
@@ -38,7 +38,7 @@ public class Agenda {
         }
 
         if (!exists) {
-            Contacto newContact = new Contacto(name, phone);
+            Persona newContact = new Persona(name, phone);
             contacts.add(newContact);
         }
     }
@@ -50,10 +50,10 @@ public class Agenda {
 
 
     public void removeContact(String name) {
-        Iterator<Contacto> it = contacts.iterator();
+        Iterator<Persona> it = contacts.iterator();
 
         while (it.hasNext()) {
-            Contacto c = it.next();
+            Persona c = it.next();
 
             if (c.getName().equalsIgnoreCase(name)) {
                 it.remove();
@@ -69,7 +69,7 @@ public class Agenda {
      */
 
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
-        for (Contacto c : contacts) {
+        for (Persona c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
                 List<String> phones = c.getPhones();
 
@@ -86,7 +86,7 @@ public class Agenda {
      *
      * @return la lista de contactos
      */
-    public List<Contacto> getContacts() {
+    public List<Persona> getContacts() {
         return this.contacts;
     }
 }
